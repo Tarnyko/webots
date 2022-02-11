2e565043abaf7aa1e46682815eb234e45f07b1fc
Manuel Bachmann <manuel.bachmann@intech.lu>
2022-02-11 08:06:41 +0100

    Support RHEL/CentOS 8 as a compilation host.

--- scripts/install/linux_compilation_dependencies.sh
+++ scripts/install/linux_compilation_dependencies.sh
@@ -5,14 +5,22 @@ if [[ $EUID -ne 0 ]]; then
        exit 1
 fi
 
-apt update
-apt install --yes git lsb-release cmake swig libglu1-mesa-dev libglib2.0-dev libfreeimage-dev libfreetype6-dev libxml2-dev libzzip-0-13 libboost-dev libgd3 libssh-gcrypt-dev libzip-dev libreadline-dev pbzip2 libpci-dev wget libssl-dev zip unzip
+. /etc/os-release
 
-UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $UBUNTU_VERSION == "20.04" ]]; then
-       apt install --yes libzip5
+if [[ $ID == "ubuntu" ]]; then
+       apt update
+       apt install --yes git lsb-release cmake swig libglu1-mesa-dev libglib2.0-dev libfreeimage-dev libfreetype6-dev libxml2-dev libzzip-0-13 libboost-dev libgd3 libssh-gcrypt-dev libzip-dev libreadline-dev pbzip2 libpci-dev wget libssl-dev zip unzip
+       if [[ $VERSION_ID == "20.04" ]]; then
+              apt install --yes libzip5
+       fi
+elif [[ $ID == @(rhel|centos|rocky|alma) ]]; then
+       dnf config-manager --set-enabled powertools
+       rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID:0:1}.noarch.rpm
+       dnf install -y git redhat-lsb-core cmake swig mesa-libGLU-devel glib2-devel freetype-devel libxml2-devel boost-devel gd-devel libssh-devel libzip-devel readline-devel pciutils-devel wget openssl-devel zip unzip java-1.8.0-openjdk-devel
+       dnf install -y freeimage-devel stb_image-devel stb_image_write-devel pbzip2
+       dnf install -y zziplib-devel glm-devel
 else
-       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
+       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS and latest RHEL/CentOS are supported."
 fi
 
 script_full_path=$(dirname "$0")
--- scripts/install/linux_runtime_dependencies.sh
+++ scripts/install/linux_runtime_dependencies.sh
@@ -5,17 +5,27 @@ if [[ $EUID -ne 0 ]]; then
        exit 1
 fi
 
-apt update
-apt install --yes lsb-release g++ make libavcodec-extra libglu1-mesa libxkbcommon-x11-dev libxcb-keysyms1 libxcb-image0 libxcb-icccm4 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcomposite-dev libxtst6 libnss3
-if [[ -z "$DISPLAY" ]]; then
-       apt install --yes xvfb
-fi
+. /etc/os-release
 
-UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $UBUNTU_VERSION == "18.04" ]]; then
-       apt install --yes ffmpeg
-elif [[ $UBUNTU_VERSION == "20.04" ]]; then
-       apt install --yes ffmpeg
+if [[ $ID == "ubuntu" ]]; then
+       apt update
+       apt install --yes lsb-release g++ make libavcodec-extra libglu1-mesa libxkbcommon-x11-dev libxcb-keysyms1 libxcb-image0 libxcb-icccm4 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcomposite-dev libxtst6 libnss3
+       if [[ $VERSION_ID == @(18.04|20.04) ]]; then
+              apt install --yes ffmpeg
+       fi
+       if [[ -z "$DISPLAY" ]]; then
+              apt install --yes xvfb
+       fi
+elif [[ $ID == @(rhel|centos|rocky|alma) ]]; then
+       dnf config-manager --set-enabled powertools
+       rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID:0:1}.noarch.rpm
+       rpm -ivh https://download1.rpmfusion.org/free/el/rpmfusion-free-release-${VERSION_ID:0:1}.noarch.rpm
+       dnf install -y redhat-lsb-core gcc-c++ make mesa-libGLU xcb-util-keysyms xcb-util-image xcb-util-wm xcb-util-renderutil libXrandr libXinerama libXcomposite-devel libXtst nss
+       dnf install -y ffmpeg
+       dnf install -y libxkbcommon-x11-devel
+       if [[ -z "$DISPLAY" ]]; then
+              dnf install -y xorg-x11-server-Xvfb
+       fi
 else
-       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
+       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS and latest RHEL/CentOS are supported."
 fi
